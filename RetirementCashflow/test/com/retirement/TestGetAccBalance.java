5cc994340aa7839f3abd01bafcf4b4d6182842d0
johnh <johnh@10.0.1.113>
2023-10-17 22:34:08 +0100

    First Commit

---
+++ RetirementCashflow/test/com/retirement/TestGetAccBalance.java
@@ -0,0 +1,21 @@
+package com.retirement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class TestGetAccBalance {
+
+    private final Account prems = new Account(50000.0,0.05);
+
+    @Test
+    void futureBal() {
+        // test balance after 15 years of inflation with no deposits 
+    	//and no withdrawals
+    	for(int i=0;i<15;i++) {
+    		prems.addInterest();
+    	}
+    	assertEquals(103946.41, prems.getdBalance(),0.1);
+    }
+
+}
---
+++ RetirementCashflow/test/com/retirement/TestWithdrawFromLowInterest.java
@@ -0,0 +1,32 @@
+package com.retirement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class TestWithdrawFromLowInterest {
+	private final Account acc1 = new Account(50000.0,0.06);
+	private final Account acc2 = new Account(50000.0,0.03);
+	private final Account acc3 = new Account(50000.0,0.04);
+	private final Account acc4 = new Account(49000.0,0.01);
+	private final Account acc5 = new Account(50000.0,0.05);
+	List<Account> Accounts = new ArrayList<>();
+
+
+	@Test
+	void test() {
+		Accounts.add(acc1);
+		Accounts.add(acc2);
+		Accounts.add(acc3);
+		Accounts.add(acc4);
+		Accounts.add(acc5);
+		Collections.sort(Accounts);
+		Accounts.get(0).withdraw(1000.0);
+		assertEquals(48000.0, Accounts.get(0).getdBalance(),0.1);
+	}
+
+}
