544eab6028524d3980c1b2fd1c8199c0e4340bd7
johnh <johnh@10.0.1.113>
2023-10-19 10:14:26 +0100

    started IncomeStream

--- RetirementCashflow/src/com/retirement/IncomeStream.java
+++ RetirementCashflow/src/com/retirement/IncomeStream.java
@@ -1,5 +1,34 @@
 package com.retirement;
 
 public class IncomeStream {
+private boolean isTimebound;
+private boolean isStarted;
+private double dStipend;
+public IncomeStream(boolean isTimebound, boolean isStarted, double dStipend) {
+	super();
+	this.isTimebound = isTimebound;
+	this.isStarted = isStarted;
+	this.dStipend = dStipend;
+}
+public boolean isTimebound() {
+	return isTimebound;
+}
+public void setTimebound(boolean isTimebound) {
+	this.isTimebound = isTimebound;
+}
+public boolean isStarted() {
+	return isStarted;
+}
+public void setStarted(boolean isStarted) {
+	this.isStarted = isStarted;
+}
+public double getdStipend() {
+	return dStipend;
+}
+public void setdStipend(double dStipend) {
+	this.dStipend = dStipend;
+}
+
+
 
 }
